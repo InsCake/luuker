cf24cf59659b48d3a8f5a533923f87030588ff57
jessiegreen0525 <450582834@qq.com>
2015-04-02 13:53:35 +0800

    update

--- app.js
+++ app.js
@@ -63,9 +63,10 @@ var express = require('express')
 var app = express()
 
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-  res.render('home');
+  res.render('pc/home');
 })
 app.get('/login', function (req, res) {
   res.send('Hello World!')
