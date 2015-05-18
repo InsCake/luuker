510eec383977c1275e4e733646d068a903cde23f
不一 <buuyii@hitour.cc>
2015-05-18 17:53:09 +0800

    update

--- routes/backstage.js
+++ routes/backstage.js
@@ -5,61 +5,50 @@ var express = require('express');
 var router = express.Router();
 var mysql = require('mysql');
 
-router.get('/', function (req, res, next) {
+router.get('/', function(req, res, next) {
     var data = {
-        page: 'home',
-        site: 'backstage',
-        header: true
+        page   : 'home',
+        site   : 'backstage',
+        header : true
     };
+    console.log(2);
     res.render('layouts/layout_backstage', data);
 });
 
 
-router.get('/sign_in', function (req, res, next) {
+router.get('/sign_in', function(req, res, next) {
     var data = {
-        page: 'sign_in',
-        site: 'backstage',
-        header: false
+        page   : 'sign_in',
+        site   : 'backstage',
+        header : false
     };
     res.render('layouts/layout_backstage', data);
 
 });
 
-router.post('/sign_in', function (req, res, next) {
-    var user = req.body.user;
-    //res.json({
-    //  msg: 'success',
-    //user: user
-    //});
+router.post('/sign_in', function(req, res, next) {
+    var user = req.body.user
 
     var connection = mysql.createConnection({
-        host: 'localhost',
-        port: '3306',
-        user: 'root',
-        password: 'root',
-        database: 'luuker'
+        host     : 'localhost',
+        port     : '3306',
+        user     : 'root',
+        password : 'root',
+        database : 'luuker'
     });
 
-    connection.query("SELECT * FROM admin WHERE name = '" + user.name + "'", function (err, rows, fields) {
-        if (err) throw err;
-        if (rows.length > 0) {
-            if (user.name == rows[0].name && user.pwd == rows[0].password) {
-
+    connection.query("SELECT * FROM admin WHERE name = '" + user.name + "'", function(err, rows, fields) {
+        if(err) throw err;
+        if(rows.length > 0) {
+            if(user.name == rows[0].name && user.pwd == rows[0].password) {
                 req.session.user = rows[0];
-    console.log(req.session.user)
-
-                res.json({
-                    msg: rows[0].name
-                });
+                console.log(1);
+                res.json({ msg : 'success' });
             } else {
-                res.json({
-                    msg: 'failed'
-                });
+                res.json({ msg : 'failed' });
             }
         } else {
-            res.json({
-                msg: 'null'
-            });
+            res.json({ msg : 'null' });
         }
     });
 
