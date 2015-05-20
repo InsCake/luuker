f98b0221fe36fa703dd08a196f546de5a6c76f11
不一 <buuyii@hitour.cc>
2015-05-20 16:14:10 +0800

    update

--- views/pages/pc/article_write/article_write.js
+++ views/pages/pc/article_write/article_write.js
@@ -1,16 +1,7 @@
 var articleWriteVM = new Vue({
     el      : '#article-write',
     data    : {
-        units    : [
-            {
-                type  : 'p-title',
-                value : '今年说什么我也要去一趟日本'
-            },
-            {
-                type  : 'text',
-                value : '在哲学上，目标是提供通过尽可能简单的API来实现的双向数据绑定和可组合的视图组件的好处。它不是一个完整的框架 - 它被设计成一个简单的灵活的视图层。你可以单独它来快速进行原型设计，也可以和其他库结合使用来自定义前端。它也是无后端服务的例如Firebase的天作之合。'
-            }
-        ],
+        units    : [],
         add_unit : {
             type  : '',
             value : ''
